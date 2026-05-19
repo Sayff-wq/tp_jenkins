4215a36ab370a5e3664c7ad7a047c3389ca66985
Sayff-wq <famdaoudipc@gmail.com>
2026-05-19 09:36:18 +0100

    Premier Script Jenkins

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Hello.py
+print("="*40)
+print("Bienvenue dans mon prmier job Jenkins")
+print("="*40)
+
+nom = input("Quel est ton nom?")
+print(f"Bonjour {nom}, ton job Jenkins a reussi !")
+
+#Petit Calcul 
+a = 10
+b = 5
+print(f"{a} + {b} = {a+b"}
+print(f"{a} - {b} = {a-b}"}
